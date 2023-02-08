3c1e90d009af65d4ba9cdb63999a093476f4d846
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-02-08 00:54:55 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function index() {
@@ -36,7 +37,9 @@ function index() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
             </div >
         </div >
     )
